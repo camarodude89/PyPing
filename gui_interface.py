42a46a7c937f2ad9473fe43c81f7017fc2588257
camarodude89 <camarodude89atm@gmail.com>
2017-01-24 15:08:00 -0500

    Made table population more dynamic

--- gui_interface.py
+++ gui_interface.py
@@ -3,11 +3,18 @@ QMainWindow, QAction, QTableWidget, QTableWidgetItem, QVBoxLayout,
 QHBoxLayout, QCheckBox)
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5 import QtCore
+import collections
 
 class GUIInterface(QWidget):
 
+    remoteMachines = [["Wet Side TV", "MAR-BPHHR22-D"],
+        ["Bowles Breakroom TV", "MSM-2Q8X821"], ["BP Breakroom TV",
+        "MAR-ABCDE12-D"], ["Pack Side Breakroom TV", "MAR-ZYX5432-L"],
+        ["Pi 3 Remote", "Things and Stuff"]]
+
     def __init__(self):
         super().__init__()
+
 
         self.initUI()
 
@@ -20,7 +27,7 @@ class GUIInterface(QWidget):
         self.createTable()
 
         self.mainLayout = QVBoxLayout()
-        self.mainLayout.addWidget(self.tableWidget)
+        self.mainLayout.addWidget(self.tW)
 
         QToolTip.setFont(QFont('SansSerif', 10))
 
@@ -43,23 +50,39 @@ class GUIInterface(QWidget):
         self.show()
 
     def createTable(self):
-        self.tableWidget = QTableWidget()
-        self.tableWidget.verticalHeader().setVisible(False)
-        self.tableWidget.setRowCount(2)
-        self.tableWidget.verticalHeaderVisible = False
-        self.tableWidget.setColumnCount(4)
-        self.tableWidget.setHorizontalHeaderLabels([' ', 'Desc', 'PC Name',
+
+        self.tW = QTableWidget()
+        self.tW.verticalHeader().setVisible(False)
+        self.tW.setRowCount(len(self.remoteMachines))
+        self.tW.verticalHeaderVisible = False
+        self.tW.setColumnCount(4)
+        self.tW.setHorizontalHeaderLabels([' ', 'Desc', 'PC Name',
         'Status'])
 
-        checkBox = QCheckBox()
-        checkBox.setChecked(True)
+        self.checkBoxList = []
 
-        self.tableWidget.setCellWidget(0,0, checkBox)
-        self.tableWidget.setItem(0,1, QTableWidgetItem("Wet Side TV"))
-        self.tableWidget.setItem(0,2, QTableWidgetItem("MAR-BPHHR22-D"))
-        self.tableWidget.setItem(0,3, QTableWidgetItem("Ok"))
-        self.tableWidget.setItem(1,1, QTableWidgetItem("Bowles Breakroom TV"))
-        self.tableWidget.setItem(1,2, QTableWidgetItem("MSM-2Q8X821"))
-        self.tableWidget.setItem(1,3, QTableWidgetItem("Unreachable"))
-        self.tableWidget.setColumnWidth(0,14)
-        self.tableWidget.move(0,0)
+        for row in range(len(self.remoteMachines)):
+
+            col = 0
+            self.checkBoxList.append(QCheckBox())
+            self.checkBoxList[row].setChecked(True)
+
+            self.tW.setCellWidget(row,col, self.checkBoxList[row])
+            self.tW.setItem(row,col+1,
+                QTableWidgetItem(self.remoteMachines[row][col]))
+            self.tW.setItem(row,col+2,
+                QTableWidgetItem(self.remoteMachines[row][col+1]))
+            self.tW.setItem(row,col+3,
+                QTableWidgetItem("Untested"))
+
+        self.tW.setColumnWidth(0,14)
+
+        #self.tableWidget.setCellWidget(0,0, checkBox)
+        #self.tableWidget.setItem(0,1, QTableWidgetItem("Wet Side TV"))
+        #self.tableWidget.setItem(0,2, QTableWidgetItem("MAR-BPHHR22-D"))
+        #self.tableWidget.setItem(0,3, QTableWidgetItem("Ok"))
+        #self.tableWidget.setItem(1,1, QTableWidgetItem("Bowles Breakroom TV"))
+        #self.tableWidget.setItem(1,2, QTableWidgetItem("MSM-2Q8X821"))
+        #self.tableWidget.setItem(1,3, QTableWidgetItem("Unreachable"))
+        #self.tableWidget.setColumnWidth(0,14)
+        #self.tableWidget.move(0,0)
